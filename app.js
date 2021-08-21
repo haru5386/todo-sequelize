d3c40bd04f105883d3b50344f6078e0a9b5d6c9b
haru5386 <snsn550@gmail.com>
2021-08-21 18:51:42 +0800

    revise:SQL todolist

--- app.js
+++ app.js
@@ -3,18 +3,42 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const session = require('express-session')
+const usePassport = require('./config/passport')
+const passport = require('passport')
+const flash = require('connect-flash')
 
 const app = express()
 const PORT = 3000
 
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+const routes = require('./routes')
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  res.send('hello world')
+usePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
 })
+
+app.use(routes)
+
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
---
+++ routes/modules/home.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../../models')
+const Todo = db.Todo
+
+
+router.get('/', (req, res) => {
+  const UserId = req.user.id
+  return Todo.findAll({
+    raw: true,
+    nest: true,
+    where: { UserId }
+  })
+    .then(todos => res.render('index', { todos }))
+    .catch((error) => { return res.status(422).json(error) })
+})
+module.exports = router
